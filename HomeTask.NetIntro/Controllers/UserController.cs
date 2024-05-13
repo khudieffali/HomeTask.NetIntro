43177cff92b5990bf3b799cb744df0e70714d46e
Ali Xudiyev <ali.xudiyev98@gmail.com>
2024-05-13 23:00:23 +0400

    hometask .net intro added

---
+++ HomeTask.NetIntro/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HomeTask.NetIntro.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.User = new string[] { "Ali", "Xudiyev", "24", "Azerbaijan", "+994553053232" };
+            ViewData["Name"] = "Hikmet";
+            ViewData["Surname"] = "Xidirzade";
+            ViewData["Age"] = 25;
+            ViewData["Country"] = "Azerbaijan";
+            ViewData["PhoneNumber"] = "+994554770076";
+            return View();
+        }
+    }
+}
